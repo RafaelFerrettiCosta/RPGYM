4c31683973233057107558b44b697fca6f5e6b38
Rafael Ferretti Costa <rafael.fcosta@sp.senac.br>
2025-08-07 15:32:57 -0300

    imagem do jogador

--- components/CircularProgressBar.tsx
+++ components/CircularProgressBar.tsx
@@ -1,7 +1,8 @@
 import Colors from '@/styles/colors';
-import React, { useEffect, useRef, useState } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, Animated } from 'react-native';
+import React, { use, useEffect, useRef, useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, Animated, Image } from 'react-native';
 import Svg, { Defs, Filter, FeDropShadow, Circle } from 'react-native-svg';
+import imgUser from '../assets/images/user.jpeg';
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
@@ -32,13 +33,12 @@ const CircularProgressBar = ({
   const [displayedLevel, setDisplayedLevel] = useState(0);
 
   useEffect(() => {
-
     const id = animatedLevel.addListener(({ value }) => {
-    setDisplayedLevel(Math.floor(value));
-  });
+      setDisplayedLevel(Math.floor(value));
+    });
 
     Animated.parallel([
-        Animated.timing(animatedProgress, {
+      Animated.timing(animatedProgress, {
         toValue: progress,
         duration: 1200,
         useNativeDriver: false,
@@ -52,7 +52,6 @@ const CircularProgressBar = ({
     return () => animatedLevel.removeListener(id);
   }, [progress, currentLevel]);
 
-
   const strokeDashoffset = animatedProgress.interpolate({
     inputRange: [0, 100],
     outputRange: [circumference, 0],
@@ -60,54 +59,61 @@ const CircularProgressBar = ({
 
   return (
     <TouchableOpacity activeOpacity={0.87} onPress={onPress}>
-    <View style={[{ width: size, height: size}, styles.container]}>
-      <Svg width={size} height={size} viewBox={`
+      <View style={[{ width: size, height: size }, styles.container]}>
+        <View style={styles.userImage}>
+          <Image
+            source={imgUser}
+            style={{ width: '100%', height: '100%', flex: 1 }}
+            resizeMode="cover"
+          />
+        </View>
+        <Svg
+          width={size}
+          height={size}
+          viewBox={`
     -${shadowPad} 
     -${shadowPad} 
-    ${size + shadowPad*2} 
-    ${size + shadowPad*2}
-  `} style={[{borderRadius: 1000}]}>
-         <Defs>
-          <Filter
-            id="dropShadow"
-            x="-50%"
-            y="-50%"
-            width="200%"
-            height="200%"
-          >
-            <FeDropShadow
-              dx={2}           // desloca 2px à direita
-              dy={2}           // desloca 2px pra baixo
-              stdDeviation={3} // desfoque
-              floodColor="black"
-              floodOpacity={0.4}
-            />
-          </Filter>
-        </Defs>
-        <Circle
-          stroke={backgroundColor}
-          fill="none"
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          strokeWidth={strokeWidth}
-        />
-        <AnimatedCircle
-          stroke={color}
-          fill="none"
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          strokeWidth={strokeWidth}
-          strokeDasharray={circumference}
-          strokeDashoffset={strokeDashoffset as any}
-          strokeLinecap="round"
-          transform={`
+    ${size + shadowPad * 2} 
+    ${size + shadowPad * 2}
+  `}
+          style={[{ borderRadius: 1000 }]}
+        >
+          <Defs>
+            <Filter id="dropShadow" x="-50%" y="-50%" width="200%" height="200%">
+              <FeDropShadow
+                dx={2} // desloca 2px à direita
+                dy={2} // desloca 2px pra baixo
+                stdDeviation={3} // desfoque
+                floodColor="black"
+                floodOpacity={0.4}
+              />
+            </Filter>
+          </Defs>
+          <Circle
+            stroke={backgroundColor}
+            fill="none"
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            strokeWidth={strokeWidth}
+            filter="url(#dropShadow)"
+          />
+          <AnimatedCircle
+            stroke={color}
+            fill="none"
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            strokeWidth={strokeWidth}
+            strokeDasharray={circumference}
+            strokeDashoffset={strokeDashoffset as any}
+            strokeLinecap="round"
+            transform={`
                 translate(${size} 0)
                 scale(-1 1)
                 rotate(20 ${size / 2} ${size / 2})
                 `}
-        />
+          />
           <Circle
             stroke={Colors.brandColor2}
             fill={Colors.brandColor2}
@@ -117,53 +123,66 @@ const CircularProgressBar = ({
             strokeWidth={4}
             filter="url(#dropShadow)"
           />
-      </Svg>
-      
-      <View style={styles.centeredText}>
-        <Text style={styles.progressText}>{`${Math.round(progress)}%`}</Text>
+        </Svg>
+
+        <View style={styles.levelContainer}>
+          <Text style={styles.levelText}>
+            <Text style={styles.span}>Lv.</Text>
+            {displayedLevel}
+          </Text>
+        </View>
       </View>
-      <View style={styles.levelContainer}>
-        <Text style={styles.levelText}><Text style={styles.span}>Lv.</Text>{displayedLevel}</Text>
-      </View>
-    </View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-    container:{
-        position:"absolute",
-        alignItems:"center",
-        borderRadius:1000,
-        justifyContent:"center",
-        bottom:-20,
-        right:10, 
-    },
+  container: {
+    position: 'absolute',
+    alignItems: 'center',
+    borderRadius: 1000,
+    justifyContent: 'center',
+    bottom: -20,
+    right: 10,
+  },
 
-    levelContainer: {
-        position: 'absolute',
-        top: 24,
-        left: -38,
-        height: '100%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
+  levelContainer: {
+    position: 'absolute',
+    top: 24,
+    left: -38,
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-    span: {
-        fontFamily: 'MontserratThin',
-        color: Colors.white,
-        fontSize: 10,
-        // fontWeight: 'bold',
-        // letterSpacing:-1,
-      },
+  span: {
+    fontFamily: 'MontserratThin',
+    color: Colors.white,
+    fontSize: 10,
+    // fontWeight: 'bold',
+    // letterSpacing:-1,
+  },
 
-    levelText: {
-        fontFamily: 'MontserratBold',
-        color: Colors.white,
-        fontSize: 16,
-        fontWeight: 'bold',
-      },
+  userImage: {
+    position: 'absolute',
+    top: 17,
+    left: 17,
+    width: 86,
+    height: 86,
+    borderRadius: 50,
+    zIndex: 0,
+    overflow: 'hidden',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+
+  levelText: {
+    fontFamily: 'MontserratBold',
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 
   centeredText: {
     position: 'absolute',
