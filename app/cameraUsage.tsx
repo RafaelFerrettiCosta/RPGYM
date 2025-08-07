d9e61b9adce09955244b9a276ae949bbf043941f
Rafael Ferretti Costa <rafael.fcosta@sp.senac.br>
2025-08-07 14:56:40 -0300

    tela de camera alfa

--- app/cameraUsage.tsx
+++ app/cameraUsage.tsx
@@ -1,8 +1,10 @@
 import HeaderlessContainer from '@/components/HeaderlessContainer';
 import {
   StyleSheet,
+  Platform,
   View,
   Text,
+  Image,
   Button,
   TouchableOpacity,
   Animated,
@@ -12,57 +14,89 @@ import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { useContext, useEffect, useRef, useState } from 'react';
 import { Easing } from 'react-native';
 import Colors from '@/styles/colors';
-
+import IconCamera from '../assets/icons/camera2.png';
+import IconFlipCameraAndroid from '../assets/icons/flipCameraAndroid.png';
+import IconFlipCameraApple from '../assets/icons/flipCameraIOS.png';
 const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
 
 export default function cameraUsage() {
   const [facing, setFacing] = useState<CameraType>('front');
   const [cameraPermission, requestCameraPermission] = useCameraPermissions();
   const [cameraReady, setCameraReady] = useState(false);
   const cameraRef = useRef<CameraView>(null);
-  const animatedWidth = useRef(new Animated.Value(10)).current;
+  // const animatedWidth = useRef(new Animated.Value(10)).current;
   const animatedHeight = useRef(new Animated.Value(10)).current;
-  const animatedBorderRadius = useRef(new Animated.Value(50)).current;
-  const translateY = useRef(new Animated.Value(200)).current;
+  const animatedBorderBottomRadius = useRef(new Animated.Value(0)).current;
+  const animatedBorderTopRadius = useRef(new Animated.Value(50)).current;
+  const animatedButtons = useRef(new Animated.Value(-80)).current;
+  // const translateY = useRef(new Animated.Value(200)).current;
 
   useEffect(() => {
-    const heightTarget = Math.floor(screenHeight * 0.7);
-    Animated.sequence([
-      Animated.parallel([
-        Animated.timing(translateY, {
-          toValue: 0, // ajuste conforme o layout
-          duration: 300,
-          easing: Easing.inOut(Easing.ease),
-          useNativeDriver: false,
-        }),
-        Animated.timing(animatedWidth, {
-          toValue: 380,
-          duration: 300,
-          easing: Easing.inOut(Easing.ease),
-          useNativeDriver: false,
-        }),
-        Animated.timing(animatedHeight, {
-          toValue: heightTarget + 30,
-          duration: 300,
-          easing: Easing.inOut(Easing.ease),
-          useNativeDriver: false,
-        }),
-      ]),
-      Animated.parallel([
-        Animated.timing(animatedWidth, {
-          toValue: 350,
-          duration: 100,
-          easing: Easing.inOut(Easing.ease),
-          useNativeDriver: false,
-        }),
-        Animated.timing(animatedHeight, {
-          toValue: heightTarget,
-          duration: 100,
-          easing: Easing.inOut(Easing.ease),
-          useNativeDriver: false,
-        }),
-      ]),
-    ]).start();
+    const heightTarget = Math.floor(screenHeight * 0.92);
+    const widthTarget = Math.floor(screenWidth * 1);
+    setTimeout(() => {
+      Animated.sequence([
+        Animated.parallel([
+          // Animated.timing(translateY, {
+          //   toValue: 0, // ajuste conforme o layout
+          //   duration: 300,
+          //   easing: Easing.inOut(Easing.ease),
+          //   useNativeDriver: false,
+          // }),
+          // Animated.timing(animatedWidth, {
+          //   toValue: widthTarget + 30,
+          //   duration: 300,
+          //   easing: Easing.inOut(Easing.ease),
+          //   useNativeDriver: false,
+          // }),
+          Animated.timing(animatedButtons, {
+            toValue: 10,
+            duration: 250,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: false,
+          }),
+          Animated.timing(animatedHeight, {
+            toValue: heightTarget + 10,
+            duration: 250,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: false,
+          }),
+          Animated.timing(animatedBorderBottomRadius, {
+            toValue: 100,
+            duration: 250,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: false,
+          }),
+          Animated.timing(animatedBorderTopRadius, {
+            toValue: 0,
+            duration: 250,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: false,
+          }),
+        ]),
+        Animated.parallel([
+          // Animated.timing(animatedWidth, {
+          //   toValue: widthTarget,
+          //   duration: 100,
+          //   easing: Easing.inOut(Easing.ease),
+          //   useNativeDriver: false,
+          // }),
+          Animated.timing(animatedHeight, {
+            toValue: heightTarget,
+            duration: 50,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: false,
+          }),
+          Animated.timing(animatedBorderBottomRadius, {
+            toValue: 50,
+            duration: 250,
+            easing: Easing.inOut(Easing.ease),
+            useNativeDriver: false,
+          }),
+        ]),
+      ]).start();
+    }, 100);
   }, []);
 
   if (!cameraPermission) {
@@ -100,25 +134,48 @@ export default function cameraUsage() {
         style={[
           styles.camera,
           {
-            width: animatedWidth,
+            // width: animatedWidth,
             height: animatedHeight,
-            borderRadius: animatedBorderRadius,
+            borderBottomLeftRadius: animatedBorderBottomRadius,
+            borderBottomRightRadius: animatedBorderBottomRadius,
           },
-          { transform: [{ translateY }] },
+          // { transform: [{ translateY }] },
         ]}
       >
         <CameraView
           ref={cameraRef}
-          style={{ flex: 1, borderRadius: 50 }}
+          style={{
+            flex: 1,
+            borderBottomLeftRadius: 50,
+            borderBottomRightRadius: 50,
+          }}
           facing={facing}
           autofocus="on"
           onCameraReady={() => setCameraReady(true)}
         ></CameraView>
       </Animated.View>
-      <View style={styles.buttonContainer}>
-        <Button title="Mudar Camera" onPress={toggleCameraFacing}></Button>
-        <Button title="Tirar Foto🔥" onPress={takePicture}></Button>
-      </View>
+      <TouchableOpacity
+        style={[styles.cameraButton, { bottom: animatedButtons }]}
+        onPress={takePicture}
+      >
+        <Image source={IconCamera} style={{ width: 48, height: 48 }} />
+      </TouchableOpacity>
+      {Platform.OS !== 'ios' && (
+        <TouchableOpacity
+          style={[styles.flipButton, { bottom: animatedButtons }]}
+          onPress={toggleCameraFacing}
+        >
+          <Image source={IconFlipCameraAndroid} style={{ width: 32, height: 32 }} />
+        </TouchableOpacity>
+      )}
+      {Platform.OS === 'ios' && (
+        <TouchableOpacity
+          style={[styles.flipButton, { bottom: animatedButtons }]}
+          onPress={toggleCameraFacing}
+        >
+          <Image source={IconFlipCameraApple} style={{ width: 32, height: 32 }} />
+        </TouchableOpacity>
+      )}
     </HeaderlessContainer>
   );
 }
@@ -129,26 +186,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     // justifyContent: 'center',
     padding: 0,
-    backgroundColor: Colors.brandColor1,
+    backgroundColor: Colors.brandColor1L5,
   },
 
   message: {},
 
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 40,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-
-    //   flexDirection: 'row',
-  },
-
   camera: {
     // flex: 1,
-    marginTop: 40,
-    borderRadius: 50,
-    width: 350,
+    // marginTop: 40,
+    // borderTopRightRadius: 0,
+    // borderTopLeftRadius: 0,
+    // borderBottomLeftRadius: 50,
+    // borderBottomRightRadius: 50,
+    width: '100%',
+    boxShadow: '0px 3px 10px rgba(0, 0, 0, 0.40)',
     // height: '90%',
+  },
+
+  cameraButton: {
+    position: 'absolute',
+    // bottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 120,
+    height: 80,
+    borderRadius: 50,
+    backgroundColor: Colors.white,
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+  },
+
+  flipButton: {
+    position: 'absolute',
+    // bottom: 22,
+    right: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: Colors.white,
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
   },
 });
