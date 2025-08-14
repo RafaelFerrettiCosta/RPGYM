ec781733f10d3ae11537bed909505309cd290e51
Rafael Ferretti Costa <rafael.fcosta@sp.senac.br>
2025-08-14 17:02:23 -0300

    wip

--- components/DashboardBox.tsx
+++ components/DashboardBox.tsx
@@ -1,37 +1,34 @@
-import { View, Text, Image, StyleSheet } from "react-native";
-import Colors from "@/styles/colors";
-import { ReactNode } from "react";
+import { View, Text, Image, StyleSheet } from 'react-native';
+import Colors from '@/styles/colors';
+import { ReactNode } from 'react';
 
 interface DashboardBoxProps {
-    children: ReactNode;
-    // buttonImg: any;
+  children: ReactNode;
+  // buttonImg: any;
 }
 
-const DashboardBox = ({children}: DashboardBoxProps) =>{
-    return(
-        <View style={styles.container}>
-            {children}
-        </View>
-    )
-}
+const DashboardBox = ({ children }: DashboardBoxProps) => {
+  return <View style={styles.container}>{children}</View>;
+};
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: 'auto',
-        gap: 10,
-        padding: 10,
-        marginBottom: 20,
-        backgroundColor: Colors.white,
-        shadowColor: '#000',         
-        shadowOffset: { width: 0, height: 4 }, 
-        shadowOpacity: 0.4,          
-        shadowRadius: 4,             
-        elevation: 3,   
-        borderRadius: 8,
-        // alignItems: 'center',
-        // justifyContent: 'center',
-    },
+  container: {
+    width: '100%',
+    height: 'auto',
+    gap: 10,
+    padding: 10,
+    paddingBottom: 20,
+    marginBottom: 20,
+    backgroundColor: Colors.white,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 3,
+    borderRadius: 8,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
 });
 
-export default DashboardBox
+export default DashboardBox;
